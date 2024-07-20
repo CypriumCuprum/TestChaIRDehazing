3dc504d5ef2e8750eaf7d2ae287a7e2ba2a18f94
CypriumCuprum <riverflowsinyou1412@gmail.com>
2024-07-20 21:43:25 +0700

    fix

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ def main(args):
     model = build_net()
     # print(model)
 
-    # if torch.cuda.device_count() > 1:
-    #     print("Let's use", torch.cuda.device_count(), "GPUs!")
-    #     # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-    #     model = DataParallel(model)
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+        model = DataParallel(model, device_ids=[0])
 
     if torch.cuda.is_available():
         model.cuda()
