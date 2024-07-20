2352d5815d2d2f9039390191c0f685717efd87f5
CypriumCuprum <riverflowsinyou1412@gmail.com>
2024-07-20 21:00:34 +0700

    fixx

--- train.py
+++ train.py
@@ -11,7 +11,8 @@ from warmup_scheduler import GradualWarmupScheduler
 
 
 def _train(model, args):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
     criterion = torch.nn.L1Loss()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, betas=(0.9, 0.999), eps=1e-8)
