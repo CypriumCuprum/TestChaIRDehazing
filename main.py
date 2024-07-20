9e0fb630519ec70754ed2946966cff9b63ef0560
CypriumCuprum <riverflowsinyou1412@gmail.com>
2024-07-20 21:36:51 +0700

    fix

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main(args):
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-        model = DataParallel(model,device_ids=[0, 1]) #in kaggle with 2 GPUs
+        model = DataParallel(model)
 
     if torch.cuda.is_available():
         model.cuda()
